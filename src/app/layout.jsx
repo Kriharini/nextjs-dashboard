2236bc7dd5a8f4a6bc524b787fd2492641a66862
Kriharini <krithika.ha@gmail.com>
2025-05-29 13:13:54 -0400

    Initial commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+/* import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,4 +14,26 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>{children}</body>
     </html>
   );
+} */
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; // Import the Inter font
+
+ 
+
+export const metadata = {
+  title: 'MO-PCCO.R3 - NextJS Financial Dashboard',
+  description:
+    'Starter code for Module 25 Capstone project - Financial Dashbard',
+};
+
+ 
+
+export default function RootLayout({
+    children,
+}) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
