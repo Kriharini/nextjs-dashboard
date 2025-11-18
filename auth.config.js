3e58e0b0e9721ac47d19628f2cf93dabddd2fe4b
Kriharini <krithika.ha@gmail.com>
2025-11-18 18:02:39 -0500

    Login and dashboard fixes

---
+++ auth.config.js
@@ -0,0 +1,21 @@
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+
+callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log("Next url",nextUrl);
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to the login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+};
